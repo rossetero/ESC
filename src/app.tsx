58a3ac8caed5d244a2e1e388eb1136ec43c8d82a
Shirely Zzyzwicz <shirelyz@un-b1.kzn.21-school.ru>
2024-04-12 17:25:26 +0300

    nice naming

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import U2k from './4u2k'
+import U2k from './4u2k_page'
 import B2B from './b2b_page'
 import Main from './main_page'
-import Price from './pricelist'
+import Price from './price_page'
 const App = () => {
   return (
     <Main />
