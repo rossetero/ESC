15b79080ecafc7b9c70746122cd3658679b2f379
Potts Lavada <pottslav@ev-r4.kzn.21-school.ru>
2024-05-15 16:12:13 +0300

    removed css and html #14 #15

--- src/components/global/global.tsx
+++
@@ -1,7 +0,0 @@
-import { GlobalStyle } from "./global.styled";
-import React from "react";
-export const StGlobal = (props) => {
-  return (
-    <GlobalStyle>{props.children}</GlobalStyle>
-  )
-};
--- src/components/global/index.ts
+++
@@ -1,1 +0,0 @@
-export { StGlobal } from './global'
