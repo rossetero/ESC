8179ded0fca2375ac36b2cdd5cf0fd6faeee0c93
lichemerka <kostrovas49@gmail.com>
2024-05-25 02:50:39 +0300

    bot added

--- src/components/client-contact-form/client-contact-form.tsx
+++ src/components/client-contact-form/client-contact-form.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState, useRef, useEffect } from "react";
 import { StyledClientContactForm } from './client-contact-form.styled'
 import InputMask from 'react-input-mask';
@@ -51,10 +52,35 @@ export const ClientContactForm: React.FC<ClientContactFormProps> = ({ isOpen, on
     };
 
     // Обработчик отправки формы
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    //     event.preventDefault();
+    //     // Ваши действия с данными формы здесь
+    //     console.log('Submitted:', { name, telegram, phone, selectedOption });
+    // };
+
+    const botToken = '6614524564:AAE27PNrRDhzXmJeyZnE0xi7l8lk3jcqStI';
+    const chatId = '969675758';
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        // Ваши действия с данными формы здесь
-        console.log('Submitted:', { name, telegram, phone, selectedOption });
+        
+        const message = `
+Имя: ${name}
+Ник в телеграме: @${telegram}
+Телефон: ${phone}
+Выбранная опция: ${selectedOption}
+        `;
+
+        try {
+            console.log('Message to send:', message);
+            await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+                chat_id: chatId,
+                text: message,
+            });
+            console.log('sent successfully.');
+        } catch (error) {
+            console.error('sendind error:', error);
+        }
     };
 
     return (
@@ -96,10 +122,12 @@ export const ClientContactForm: React.FC<ClientContactFormProps> = ({ isOpen, on
             <div>
                 <label htmlFor="selectOption">Я хочу.. (выбери одну из опций)</label>
                 <select className="input" value={selectedOption} onChange={handleSelectChange}>
-                    <option value="1">500₽ - один раз</option>
-                    <option value="2">900₽ - проходка на двоих</option>
-                    <option value="3">2200₽ - абонемент на 5 встреч</option>
-                    <option value="4">5000₽ - личный спикинг клаб</option>
+                    <option value="разовое посещение">500₽ - один раз</option>
+                    <option value="на двоих">900₽ - проходка на двоих</option>
+                    <option value="абонемент на 5">2200₽ - абонемент на 5 встреч</option>
+                    <option value="privat esc">5000₽ - личный спикинг клаб</option>
+                    <option value="инвайт">бесценно - пригласть esc к вам</option>
+                    <option value="вопрос">мне только спросить!</option>
                 </select>
             </div>
             <button>Отправить</button>
