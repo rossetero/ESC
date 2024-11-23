077245b59c72e92443555e752ba5b0d91d64b778
DmitrySR <q1309q@gmail.com>
2024-11-23 15:47:49 +0300

    transfer

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,57 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+  },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
+  {
+    ignores: ['stubs/', 'bro.config.js'],
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig, {
-    rules: {
-      'react/display-name': 'off',
-      'react/jsx-key': 'off',
-      'react/prop-types': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        { argsIgnorePattern: '^_', caughtErrors: 'none' },
-      ],
-      '@typescript-eslint/ban-types': 'off',
-      '@typescript-eslint/no-var-requires': 'off',
-      "@typescript-eslint/no-explicit-any": "off"
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      '@stylistic': stylistic,
     },
-
-  }
-];
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn', // or "error"
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: false,
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: true,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: true,
+        },
+      ],
+      semi: ['error', 'never'],
+      '@stylistic/indent': ['error', 2],
+    },
+  },
+]
