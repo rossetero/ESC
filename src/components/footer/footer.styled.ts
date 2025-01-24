3af2327d19b6ecd78592dac195db9e28bd7eb845
Svetlana Kostrova <kostrovas49@Svetlanas-MacBook-Air.local>
2025-01-23 16:13:23 -0800

    footer adaptive style

--- src/components/footer/footer.styled.ts
+++ src/components/footer/footer.styled.ts
@@ -13,14 +13,48 @@ export const StyledFooter = styled.footer<StyledFooterProps>`
     justify-content: space-between;
     font-size: 1vw;
     margin-top: ${props => (props.noMargin ? '0%' : '5%')};
-    .contact{
-        display:block;
+    
+    .contact {
+        display: block;
     }
+    
     .copyright {
-    padding-left: 10px;
-    font-weight: 500;
-    font-size: 12px;
-    letter-spacing: 0.05em;
-    color: rgba(0, 0, 0, 0.5);
-}
+        padding-left: 10px;
+        font-weight: 500;
+        font-size: 12px;
+        letter-spacing: 0.05em;
+        color: rgba(0, 0, 0, 0.5);
+    }
+
+    @media (max-width: 768px) {
+        height: auto;
+        padding: 30px 46px;
+        flex-direction: column;
+        gap: 20px;
+        font-size: 3vw;
+
+        .copyright {
+            font-size: 2.5vw;
+            padding-left: 0;
+            text-align: center;
+        }
+
+        .contact {
+            text-align: center;
+            li {
+                margin-bottom: 10px;
+            }
+        }
+
+        div {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 10px;
+        }
+
+        nav {
+            width: 100%;
+        }
+    }
 `;
