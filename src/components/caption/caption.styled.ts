f7215e24638c46bc63fe6447379e5e0861649236
Svetlana Kostrova <kostrovas49@Svetlanas-MacBook-Air.local>
2025-01-24 23:22:47 -0800

    price page adaptive style

--- src/components/caption/caption.styled.ts
+++ src/components/caption/caption.styled.ts
@@ -4,29 +4,36 @@ import { css } from '@emotion/react'
 
 export const StyledCaption = styled.div`
     h1{
-    font-weight: 900;
-    font-size: ${(props: any) => (props.footer ? '5vw' : '7vw')};
-    letter-spacing: 0.05em;
+        font-weight: 900;
+        font-size: ${(props: any) => (props.footer ? '5vw' : '7vw')};
+        letter-spacing: 0.05em;
     }
+
     span{
         color: var(--text-red);
     }
 
     h2 {
-    font-weight: 900;
-    font-size: 2vw;
-    letter-spacing: 0.05em;
-    text-align: justify;
-}
+        font-weight: 900;
+        font-size: 2vw;
+        letter-spacing: 0.05em;
+        text-align: justify;
+    }
 
+    @media (max-width: 768px) {
+        h1 {
+            padding-top: 30px;
+            padding-bottom: 30px;
+        }
+    }
 
     ${(props: any) => {
         if (props.hide) {
             return css`
     @media (max-width: 600px) {
-    {
-        display: none;
-    }
+        {
+            display: none;
+        }
     }
 `
         }
