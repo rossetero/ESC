6392415fd67e419e532fb075e04a728dbe2de84a
DmitrySR <q1309q@gmail.com>
2025-02-08 10:28:58 +0300

    another

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,7 @@ export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
   {
-    ignores: ['stubs/', 'bro.config.js','src/assets/','locales/'],
+    ignores: ['stubs/', 'bro.config.js'],
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
